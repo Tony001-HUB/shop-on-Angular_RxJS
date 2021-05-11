654ed5a3b6759991d2df2994cc79a1df099285bd
Tony <anton.acipenka@gmail.com>
2021-05-11 20:03:08 +0300

    Added user components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import {MainPageComponent} from './shared/main-page/main-page.component';
+import {ProductPageComponent} from './shared/product-page/product-page.component';
+import {CartPageComponent} from './shared/cart-page/cart-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full'},
+      { path: '', component: MainPageComponent },
+
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'cart', component: CartPageComponent }
+    ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './shared/main-page/main-page.component';
+import { ProductPageComponent } from './shared/product-page/product-page.component';
+import { CartPageComponent } from './shared/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
